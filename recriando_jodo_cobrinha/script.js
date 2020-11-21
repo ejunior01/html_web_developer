275c563e287c5851c51d89b9f7815f421622cb78
Edvan Ribeiro <jk.jr47@gmail.com>
2020-11-21 07:11:02 -0300

    finalizando jogo

--- recriando_jodo_cobrinha/script.js
+++ recriando_jodo_cobrinha/script.js
@@ -8,12 +8,16 @@ snake[0] = {
   y: 8 * box,
 };
 
+function randomFood() {
+  return Math.floor(Math.random() * 15 + 1) * box;
+}
+
 let food = {
-  x: Math.floor(Math.random() * 15 + 1) * box,
-  y: Math.floor(Math.random() * 15 + 1) * box,
+  x: randomFood(),
+  y: randomFood(),
 };
 
-let direction = "right";
+let direction = "left";
 
 function createBG() {
   context.fillStyle = "lightgreen";
@@ -41,11 +45,23 @@ function update(event) {
 
 document.addEventListener("keydown", update);
 
+function gameOver() {
+  clearInterval(game);
+  alert("ok");
+  location.reload();
+}
+
 function startGame() {
-  if (snake[0].x > 15 * box && direction === "right") snake[0].x = 0;
-  if (snake[0].x < 0 && direction === "left") snake[0].x = 16 * box;
-  if (snake[0].y > 15 * box && direction === "up") snake[0].y = 0;
-  if (snake[0].y < 0 && direction === "down") snake[0].y = 16 * box;
+  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+      gameOver();
+    }
+  }
 
   createBG();
   createSnake();
@@ -59,7 +75,12 @@ function startGame() {
   if (direction == "up") snakeY -= box;
   if (direction == "down") snakeY += box;
 
-  snake.pop();
+  if (snakeX != food.x || snakeY != food.y) {
+    snake.pop();
+  } else {
+    food.x = randomFood();
+    food.y = randomFood();
+  }
 
   let newHead = {
     x: snakeX,
@@ -69,4 +90,4 @@ function startGame() {
   snake.unshift(newHead);
 }
 
-let game = setInterval(startGame, 150);
+let game = setInterval(startGame, 200);
